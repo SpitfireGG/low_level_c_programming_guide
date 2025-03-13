8aff55757241966948a63aeec3514430c7c75a14
SpitfireGG <raambasnet02@gmail.com>
2025-03-13 21:26:10 +0545

    pointers hacks

---
+++ src/basics/pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Pointers
+
+/*
+ reading pointers form right to left is how it is done
+ ex : int *arr ; --> defines a arr that is a pointer to an int
+*/
+
+// learn more about pointers in
+// https://www.spsanderson.com/steveondata/posts/2025-03-05/
+
+int main() { return 0; }
