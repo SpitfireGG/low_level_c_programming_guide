c24280819a87966d379c43b7028b4e2fee67593e
SpitfireGG <raambasnet02@gmail.com>
2025-03-30 00:21:02 +0545

    stack impl

---
+++ dsa/stack_queue/main.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 10;
+    int y = ++x;
+    printf("x= %d\n", x);
+    printf("y= %d\n", y);
+    return 0;
+}
