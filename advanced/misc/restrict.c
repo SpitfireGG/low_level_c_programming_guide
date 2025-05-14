2efc08f42c301c25dbddc6305e3f087d6316cef2
SpitfireGG <raambasnet02@gmail.com>
2025-05-14 19:33:09 +0545

    add: benchmark on higher iterations

--- advanced/misc/restrict.c
+++ advanced/misc/restrict.c
@@ -1,65 +1,155 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include "bits/time.h"
 
-void updatePtrs(size_t *ptrA, size_t *ptrB, size_t *val)
+#define ITERATIONS 1000
+#define ARRAY_SIZE 10000
+
+double no_restrict(size_t n, double *x, double *y, double *result)
 {
-    *ptrA += *val;
-    *ptrB += *val;
-    printf("unrestricted : %ld + %ld = %ld\n", *ptrA, *ptrB, *ptrA + *ptrB);
+    double sum = 0.0;
+    for (size_t i = 0; i < n; i++)
+    {
+        result[i] = x[i] + y[i];
+        sum += result[i];
+    }
+    return sum;
 }
 
-void updatePtrsRestrict(size_t *restrict ptrA,
-                        size_t *restrict ptrB,
-                        size_t *restrict val)
+double use_restrict(size_t n,
+                    double *restrict x,
+                    double *restrict y,
+                    double *restrict result)
 {
-    *ptrA += *val;
-    *ptrB += *val;
-    printf("restricted : %ld + %ld = %ld\n", *ptrA, *ptrB, *ptrA + *ptrB);
+    double sum = 0.0;
+    for (size_t i = 0; i < n; i++)
+    {
+        result[i] = x[i] + y[i];
+        sum += result[i];
+    }
+    return sum;
 }
 
-double get_elapsed_time(struct timespec start, struct timespec end)
+double elapsed_time(struct timespec start, struct timespec end)
 {
-    return ((end.tv_sec - start.tv_sec) * 1e3)
-           + ((end.tv_nsec - start.tv_nsec) / 1e-9);
-}
+    double sec_diff = (end.tv_sec - start.tv_sec) * 1e3;
 
-void benchmark()
+    double nsec_diff = (end.tv_nsec - start.tv_nsec) / 1e6;
+
+    // check if time_nsec is negative , if negative we add 1 milisecond to
+    // time_sec
+    if (end.tv_nsec < start.tv_nsec)
+    {
+        sec_diff -= 1.0;
+        nsec_diff += 1000.00;
+    }
+    return sec_diff + nsec_diff;
+}
+void get_benchmark()
 {
     struct timespec start, end;
+    double unrestricted_total = 0.0;
+    double restricted_total   = 0.0;
 
-    // init vars
-    size_t a   = 10;
-    size_t b   = 30;
-    size_t val = 40;
+    // allocate memory
+    double *x    = malloc(ARRAY_SIZE * sizeof(double));
+    double *y    = malloc(ARRAY_SIZE * sizeof(double));
+    double *res1 = malloc(ARRAY_SIZE * sizeof(double));
+    double *res2 = malloc(ARRAY_SIZE * sizeof(double));
 
-    printf("running unrestricted !!!!\n\n");
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    updatePtrs(&a, &b, &val);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    double unrestricted_elapsed = get_elapsed_time(start, end);
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    updatePtrsRestrict(&a, &b, &val);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    double resticted_elapsed = get_elapsed_time(start, end);
-
-    if (resticted_elapsed < unrestricted_elapsed)
+    if (!x || !y || !res1 || !res2)
     {
-        printf("\n............benchmark...........\n\n");
-        printf("unrestricted took:  %.2fms\n", unrestricted_elapsed);
-        printf("restricted took: %.2fms\n", resticted_elapsed);
+        perror("allocation failed\n");
+        exit(1);
+    }
+
+    // init the array with some data
+    for (size_t i = 0; i < ARRAY_SIZE; i++)
+    {
+        x[i]    = (double) i;
+        y[i]    = (double) (ARRAY_SIZE - i);
+        res1[i] = 0.0;
+        res2[i] = 0.0;
+    }
+
+    printf("running the benchmark on [ %d iterations ] on [ %d array "
+           "size ].....\n\n",
+           ITERATIONS,
+           ARRAY_SIZE);
+
+    printf("runnning the benchmark on  unrestricted...\n");
+
+    // for unrestricted
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        memset(res1, 0, sizeof(double) * ARRAY_SIZE);
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        no_restrict(ARRAY_SIZE, x, y, res1);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        unrestricted_total += elapsed_time(start, end);
+    }
+    double unrestricted_avg = (unrestricted_total / ITERATIONS);
+    printf("average time by unrestricted : %.6fms\n", unrestricted_avg);
+
+    // for restricted
+    printf("\nrunnning the benchmark on restricted...\n");
+
+    // for unrestricted
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        // use the same variable for each iterations
+        memset(res2, 0, sizeof(double) * ARRAY_SIZE);
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        use_restrict(ARRAY_SIZE, x, y, res2);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        restricted_total += elapsed_time(start, end);
+    }
+
+    // get the average time diff of both of them
+
+    double restricted_avg = (restricted_total / ITERATIONS);
+    printf("average time by restricted : %.6fms\n", restricted_avg);
+
+    // check if one of them has higher time diff
+
+    if (restricted_avg < unrestricted_avg)
+    {
+        double time_diff  = unrestricted_avg - restricted_avg;
+        double diff_ratio = unrestricted_avg / restricted_avg;
+
+        printf("for [ %d iterations ] restricted performed better by diff: "
+               "%.6fms (%2.fx speedup)\n",
+               ITERATIONS,
+               time_diff,
+               diff_ratio);
+    }
+    else if (unrestricted_avg < restricted_avg)
+    {
+        double time_diff  = restricted_avg - unrestricted_avg;
+        double diff_ratio = restricted_avg / unrestricted_avg;
+        printf("for [ %d iterations ] unrestricted performed better by diff: "
+               "%.6fms\nratio diff: %.6fms",
+               ITERATIONS,
+               time_diff,
+               diff_ratio);
     }
     else
     {
-        printf("restricted took more time than unrestricted??\n\n");
-        perror("benchmark");
+        printf("both of them took the same time for %d\n", ITERATIONS);
     }
+    free(x);
+    free(y);
+    free(res1);
+    free(res2);
 }
 
 int main()
 {
-    benchmark();
+    get_benchmark();
     return 0;
 }
