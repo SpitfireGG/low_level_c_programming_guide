57a7168037b875dcf618cf032bb850909e1b6502
SpitfireGG <raambasnet02@gmail.com>
2025-05-26 22:29:02 +0545

    Move asm files to asm_prog/ch01

--- ch01/exit.asm
+++
@@ -1,9 +0,0 @@
-section .text
-    global _start
-
-_start:
-    mov rax, 60 ;; syscall for exit
-    mov rsi, 0 ;;  is the first argument to the exit syscall, o means success like in c
-    syscall
-
-
